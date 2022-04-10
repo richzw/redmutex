ca9ce262431b564507c6ac20e77b3ee07ae29548
Wei Zang <richzw@gmail.com>
2022-04-10 08:07:30 +0800

    feat(script): update lua script refer to Redission

--- lua.go
+++ lua.go
@@ -4,7 +4,6 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-
 var DelScript = redis.NewScript(`
 	if redis.call("GET", KEYS[1]) == ARGV[1] then
 		return redis.call("DEL", KEYS[1])
@@ -12,7 +11,6 @@ var DelScript = redis.NewScript(`
 		return 0
 	end
 `)
-
 
 var ExpScript = redis.NewScript(`
 	if redis.call("GET", KEYS[1]) == ARGV[1] then
@@ -28,4 +26,46 @@ var PTTLScript = redis.NewScript(`
 	else 
 		return -1
 	end
+`)
+
+var GetScript = redis.NewScript(`
+	if (redis.call('exists', KEYS[1]) == 0) then
+	  redis.call('hincrby', KEYS[1], ARGV[2], 1);
+	  redis.call('pexpire', KEYS[1], ARGV[1]);
+	  return nil;
+	end;
+	
+	if (redis.call('hexists', KEYS[1], ARGV[2]) == 1) then
+	  redis.call('hincrby', KEYS[1], ARGV[2], 1);
+	  redis.call('pexpire', KEYS[1], ARGV[1]);
+	  return nil;
+	end;
+	
+	return redis.call('pttl', KEYS[1]);
+`)
+
+var DelScript = redis.NewScript(`
+	if (redis.call('hexists', KEYS[1], ARGV[3]) == 0) then
+	 return nil;
+	end;
+	
+	local counter = redis.call('hincrby', KEYS[1], ARGV[3], -1); 
+	
+	if (counter > 0) then
+	 redis.call('pexpire', KEYS[1], ARGV[2]);
+	return 0;
+	else
+	 redis.call('del', KEYS[1]);
+	 redis.call('publish', KEYS[2], ARGV[1]);
+	return 1;
+	end;
+	return nil;
+`)
+
+var RefreshScript = redis.NewScript(`
+	if (redis.call('hexists', KEYS[1], ARGV[2]) == 1) then 
+	 redis.call('pexpire', KEYS[1], ARGV[1]); 
+	  return 1; 
+	end;  
+	return 0;
 `)
